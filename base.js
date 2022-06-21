f4f9897f8c11b8900744cce4f47981cdd33468c5
chris_yooo <hhxtreams@gmail.com>
2022-06-21 15:23:06 +0200

    first init

---
+++ base.js
@@ -0,0 +1,46 @@
+const cardsContainer = document.querySelector("[data-js=cards]");
+const form = document.querySelector("[data-js=form]");
+
+let cards = [];
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const questionElement = form.elements.question;
+  const answerElement = form.elements.answer;
+  const tagsElement = form.elements.tags;
+
+  const newCard = {
+    question: questionElement.value,
+    answer: answerElement.value,
+    tags: tagsElement.value
+      .split(",")
+      .map((tag) => tag.trim())
+      .filter((tag) => tag.length),
+  };
+
+  cards = [newCard, ...cards];
+  renderCards();
+
+  form.reset();
+  // this moves the focus and is considered bad practice:
+  // questionElement.focus();
+});
+
+function renderCards() {
+  cardsContainer.ariaBusy = "true";
+  cardsContainer.innerHTML = "";
+  cards.forEach((card) => {
+    const cardElement = document.createElement("li");
+    cardElement.className = "card";
+    cardElement.innerHTML = `
+      <p>${card.question}</p>
+      <p>${card.answer}</p>
+      <ul role="list" class="card__tag-list">
+        ${card.tags.map((tag) => `<li class="card__tag">${tag}</li>`).join("")}
+      </ul>
+    `;
+    cardsContainer.append(cardElement);
+  });
+  cardsContainer.ariaBusy = "false";
+}
